9a0295d5a2cfa9ab07f51cdd2a8f21d9054c986e
Anannya Patra <40665106+anannya03@users.noreply.github.com>
2024-11-14 02:12:35 -0800

    Update Header.jsx

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className="h-[72px] px-4 flex items-center justify-between sticky top-0 z-20 bg-blue backdrop-blur-md">
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/Anannya_Patra_SDE_Resume.pdf")}>
+      <OutlineButton onClick={() => window.open("/AnannyaPatra_SDE_Resume_.pdf")}>
         My Resume
       </OutlineButton>
     </header>
